a86ed4cb7190accde9dc6337ecff71da5cef6e8b
murdadrum <me@murdadrum.com>
2023-11-28 01:09:21 -0600

    update file names, preface with 3 digit

---
+++ 010InsertBasics.sql
@@ -0,0 +1,13 @@
+-- CODE: INSERT: The Basics
+-- CREATE TABLE Tweets (
+-- Username VARCHAR(15),
+-- Content VARCHAR(144),
+-- Favorites INT
+-- );
+
+INSERT INTO Tweets (Username, Content, Favorites)
+VALUES ('ana', 'RE!SPEK!!', 3000);
+
+-- Another 
+INSERT INTO Tweets (Username, Content, Favorites)
+VALUES ('ana', 'RE!SPEK!!', 3000);
