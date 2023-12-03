f69dfe35c53c369cd3a20d110508fb2d8b20ce71
murdadrum <me@murdadrum.com>
2023-12-03 08:18:22 -0600

    move SQLs to root SQL folder

--- 001Fam/019CreateInsertExFINAL.sql
+++ 001Fam/019CreateInsertExFINAL.sql
@@ -1,7 +1,6 @@
 CREATE database FamilyCopy;
 USE FamilyCopy;
 SELECT database();
-
 CREATE TABLE MembersCopy (
     memberid INT AUTO_INCREMENT PRIMARY KEY,
     lastname VARCHAR(50) NOT NULL DEFAULT 'Barteaux',
@@ -9,28 +8,24 @@ CREATE TABLE MembersCopy (
     middlename VARCHAR(50),
     age INT NOT NULL,
     location VARCHAR(50) NOT NULL DEFAULT 'Oklahoma'
-); 
-
+);
 DESC MembersCopy;
-
 INSERT INTO MembersCopy (lastname, firstname, middlename, age, location)
 VALUES ('Barteaux', 'Luke', 'Ed', 40, 'Petit Bay, OK'),
-(lastname, 'Grayson', 'Luke', 9, 'Petit Bay, OK'),
-('Long', 'James', 'Lacey', 65, 'Park Hill, OK'),
-('Long', 'Lisa', NULL, 70, 'Park Hill, OK'),
-(lastname, 'Sarah', 'Marie', 38, location),
-(lastname, 'Josh', 'Lowery', 44, 'St Thomas, USVI'),
-(lastname, 'Steph', NULL, 48, location)
-;
-
+    (lastname, 'Grayson', 'Luke', 9, 'Petit Bay, OK'),
+    ('Long', 'James', 'Lacey', 65, 'Park Hill, OK'),
+    ('Long', 'Lisa', NULL, 70, 'Park Hill, OK'),
+    (lastname, 'Sarah', 'Marie', 38, location),
+    (
+        lastname,
+        'Josh',
+        'Lowery',
+        44,
+        'St Thomas, USVI'
+    ),
+    (lastname, 'Steph', NULL, 48, location);
 SHOW databases;
-
 USE FamilyCopy;
-
 SELECT FamilyCopy;
-
 SHOW Tables;
-
 SELECT * FROM MembersCopy;
-
--- test sync
