54a057e373119181d5b40b4f098bdd23048b1f9c
murdadrum <me@murdadrum.com>
2023-12-06 08:03:28 -0600

    dates times

---
+++ 007DataTypeContinued/CHARvsVARCHAR.sql
@@ -0,0 +1,11 @@
+-- data types reference https://dev.mysql.com/doc/refman/8.0/en/data-types.html
+
+-- The length of a CHAR column is fixed to the 
+-- length that you declare when you create the table. 
+-- The length can be any value from 0 to 255. 
+-- When CHAR values are stored, they are right-padded 
+-- with spaces to the specified length. When CHAR 
+-- values are retrieved, trailing spaces are removed 
+-- unless the PAD_CHAR_TO_FULL_LENGTH SQL mode is enabled.
+
+
