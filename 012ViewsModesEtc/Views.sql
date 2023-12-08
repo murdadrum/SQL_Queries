2059778638c6d248ffde187b5d810d601eca4839
murdadrum <me@murdadrum.com>
2023-12-08 00:18:16 -0600

    Views and MODES

---
+++ 012ViewsModesEtc/Views.sql
@@ -0,0 +1,77 @@
+-- CREATE VIEW
+-- INSTEAD OF TYPING THIS QUERY ALL THE TIME...
+SELECT 
+    title, released_year, genre, rating, first_name, last_name
+FROM
+    reviews
+        JOIN
+    series ON series.id = reviews.series_id
+        JOIN
+    reviewers ON reviewers.id = reviews.reviewer_id;
+ 
+-- WE CAN CREATE A VIEW:
+CREATE VIEW full_reviews AS
+SELECT title, released_year, genre, rating, first_name, last_name FROM reviews
+JOIN series ON series.id = reviews.series_id
+JOIN reviewers ON reviewers.id = reviews.reviewer_id;
+ 
+-- NOW WE CAN TREAT THAT VIEW AS A VIRTUAL TABLE 
+-- (AT LEAST WHEN IT COMES TO SELECTING)
+SELECT genre, AVG(rating) FROM full_reviews GROUP BY genre;
+
+-- updateable views
+-- replacing/altering views
+CREATE VIEW ordered_series AS
+SELECT * FROM series ORDER BY released_year;
+ 
+CREATE OR REPLACE VIEW ordered_series AS
+SELECT * FROM series ORDER BY released_year DESC;
+ 
+ALTER VIEW ordered_series AS
+SELECT * FROM series ORDER BY id;
+
+SELECT * FROM ordered_series;
+ 
+DROP VIEW ordered_series;
+
+-- GROUP BY HAVING clause
+-- use HAVING after group, WHERE is part of the select filter
+SELECT 
+    title, 
+    AVG(rating),
+    COUNT(rating) AS review_count
+FROM full_reviews 
+GROUP BY title HAVING COUNT(rating) > 1;
+
+-- WITH ROLLUP only works with GROUP UP include row for entire TABLE avg, sum, etc
+SELECT 
+    title, AVG(rating)
+FROM
+    full_reviews
+GROUP BY title WITH ROLLUP;
+ 
+ 
+SELECT 
+    title, COUNT(rating)
+FROM
+    full_reviews
+GROUP BY title WITH ROLLUP;
+ 
+ 
+SELECT 
+    first_name, released_year, genre, AVG(rating)
+FROM
+    full_reviews
+GROUP BY released_year , genre , first_name WITH ROLLUP;
+
+-- SQL MODE basics
+-- To View Modes:
+SELECT @@GLOBAL.sql_mode;
+SELECT @@SESSION.sql_mode;
+ 
+-- To Set Them:
+SET GLOBAL sql_mode = 'modes';
+SET SESSION sql_mode = 'modes';
+
+-- MODE STRICT_TRANS_TABLES
+
