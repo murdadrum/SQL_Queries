4bc4239c2e78cb612517b880b7b88b1b9f7eea01
murdadrum <me@murdadrum.com>
2024-02-26 18:50:28 -0400

    Reviewed, commit.

---
+++ QUERY NOTES/queries.sql
@@ -0,0 +1,11 @@
+-- SHOW databases;
+SHOW databases;
+
+-- CREATE DATABASE soap_store;
+CREATE DATABASE <database-name>;
+
+-- DROP DATABASE <database-name>;
+DROP DATABASE <database-name>;
+
+-- USE <database-name>;
+USE <database-name>;
